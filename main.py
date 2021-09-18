ee7eb1f27fab9430233861726973d38969775251
Erik Schauer <do1ffe@darc.de>
2021-09-18 02:11:19 +0200

    v0.1h

--- main.py
+++ main.py
@@ -1,49 +1,27 @@
-import os
+# import os
 import tkinter.font as font
 from tkinter import *
-import serial
-import smtplib
-from email.message import EmailMessage
-import time
-import dotenv
+from serial import *
 from settings import *
 
-__version__ = '0.1g'
+__version__ = '0.1h'
+
 #
-# Einstellungen ausschließlich in der Datei settings.py und .env machen!
+# Einstellungen ausschließlich in der Datei settings.py machen!
 #
-ser = serial.Serial(com_port, baudrate, timeout=1)
-dotenv.load_dotenv()
+#
+# COM-Port definieren
+ser = Serial(com_port, baudrate, timeout=1)
 
-
-# Email Alerts
-def email_alert(body):
-    now = time.strftime("%d.%m.%Y %H:%M:%S - ", time.localtime(time.time()))
-    body = str(now+body)
-    msg = EmailMessage()
-    msg.set_content(body)
-    msg['subject'] = 'QO-100 Alert!'
-    msg['to'] = 'do1ffe@darc.de'
-
-    user = os.getenv('user')
-    password = os.getenv('password')
-
-    msg['from'] = f"DL0HDB-Alert <{user}>"
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(user, password)
-    server.send_message(msg)
-    server.quit()
-
-
-email_alert("Das QO-100 Programm wurde gestartet...")
-
+# Versuchen, COM-Port zu öffnen
 try:
     ser.open()
+# Wenn COM-Port schon offen, dann einmal schließen und neu öffnen.
 except:
     ser.close()
     ser.open()
 
+# GUI erstellen
 root = Tk()
 root.title("QO-100 v" + __version__)
 # root.geometry("300x150")
@@ -52,16 +30,13 @@ root.resizable(width=False, height=False)
 
 # Prozedur für QO-100 Button
 def qo100():
-    email_alert("Der QO-100 Button wurde benutzt...")
-    # print(ser.name)
     qo100list = ''
     for command in qo100_cat:
         qo100list = qo100list + command + ';'
 
     # ser.write(b'FA432431000;OS00;CT00;MD02;PC005;')
-    ser.write(b + qo100list)
-    # ret = ser.readline()
-    # print(ret)
+    ser.write(bytes(qo100list.encode('ascii')))
+
     label2.config(text="QO-100 Betrieb geschaltet!")
     console_button.config(state=ACTIVE)
     qo100_button.config(state=DISABLED)
@@ -70,24 +45,21 @@ def qo100():
 
 # Prozedur für Normal Button
 def normal():
-    email_alert("Der NORMAL Button wurde benutzt...")
-    # print(ser.name)
     normallist = ''
     for command in normal_cat:
         normallist = normallist + command + ';'
 
     # ser.write(b'MC003;PC010;')
-    ser.write(b + normallist)
-    # ret = ser.readline()
-    # print(ret)
+    ser.write(bytes(normallist.encode('ascii')))
+
     label2.config(text="NORMAL Betrieb geschaltet!")
     console_button.config(state=DISABLED)
     normal_button.config(state=DISABLED)
     qo100_button.config(state=ACTIVE)
 
 
+# Prozedur für Start der SDR-Console
 def sdr_console():
-    email_alert("Die SDR-Console wurde gestartet...")
     console_button.config(state=DISABLED)
     normal_button.config(state=DISABLED)
     qo100_button.config(state=DISABLED)
@@ -102,7 +74,7 @@ tabellenbreite = 3
 
 # Copyright-Label
 label1 = Label(root, text="QO-100 Steuerung v" + __version__)
-label1_2 = Label(root, text="Software: \xa9 08/2021 by DO1FFE\nHardware: \xa9 08/2021 by DO7GJ")
+label1_2 = Label(root, text="Software: \xa9 09/2021 by DO1FFE\nHardware: \xa9 08/2021 by DO7GJ")
 myFont = font.Font(size=20)
 label1['font'] = myFont
 label1.grid(row=0, column=0, columnspan=tabellenbreite)
@@ -113,22 +85,21 @@ normal_button = Button(root, text="Normal", pady=10, padx=50, command=normal)
 normal_button.config(state=DISABLED)
 normal_button.grid(row=2, column=0, pady=10, padx=10)
 
-
 # Button für QO-100 Betrieb
 qo100_button = Button(root, text="QO-100", pady=10, padx=50, command=qo100)
 qo100_button.config(state=ACTIVE)
 qo100_button.grid(row=2, column=1, padx=10)
-
 
 # Button für SDR-Console starten
 console_button = Button(root, text="SDR-Console", pady=10, padx=50, command=sdr_console)
 console_button.config(state=DISABLED)
 console_button.grid(row=2, column=2, padx=10)
 
-
+# Hinweistexte
 label2 = Label(root, text="Bitte Betriebsmodus wählen!")
 label2.grid(row=3, column=0, columnspan=tabellenbreite, pady=10)
 
+# Programmschleife
 root.mainloop()
+# COM-Port wieder schließen
 ser.close()
-email_alert("Das QO-100 Programm wurde beendet...")
--- settings.py
+++ settings.py
@@ -1,19 +1,23 @@
 # Settings für QO-100 Programm
 #
-com_port = 'COM1'   # COM-Port für CAT, der verwendet werden soll.
-baudrate = 9600     # Baudrate für CAT
-
+com_port = 'COM1'  # COM-Port für CAT, der verwendet werden soll.
+baudrate = 9600  # Baudrate für CAT
 
 qo100_cat = {
-#    CAT :  Beschreibung
+    # CAT-Befehl :  Beschreibung
     'FA432431000': 'Frequenz auf VFO-A 432,431MHz',
     'OS00': 'Simplexbetrieb',
     'CT00': 'CTCSS AUS',
-    'MD02' : 'MODE auf USB',
-    'PC005' : 'Sendeleistung auf 5W'
+    'MD02': 'MODE auf USB',
+    'PC005': 'Sendeleistung auf 5W',
+    'EX140010' : 'Maximale Sendeleistung auf 10W begrenzen',
+    'LK1' : 'LOCK einschalten'
 }
 
 normal_cat = {
-    'MC003' : 'Memory Channel 003',
-    'PC010' : 'Sendeleistung auf 10W'
+    # CAT-Befehl :  Beschreibung
+    'MC003': 'Memory Channel 003',
+    'PC010': 'Sendeleistung auf 10W',
+    'EX140020' : 'Maximale Sendeleistung auf 20W begrenzen',
+    'LK1' : 'LOCK einschalten'
 }
